349b90f1d3f57aa453a681fc7535e8e86102c98a
Дарья Сутормина < @DESKTOP-5B3U95N>
2017-03-14 10:16:41 +0300

    Add project files.

---
+++ банки1/Card.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace банки1
+{
+    class Card
+    {
+        private string _holrer_name;
+
+        public string Holrer_name
+        {
+            get { return _holrer_name; }
+            set { _holrer_name = value; }
+        }
+        private double _number;
+
+        public double Number
+        {
+            get { return _number; }
+            set { _number = value; }
+        }
+        private int _cvc;
+
+        public int Cvc
+        {
+            get { return _cvc; }
+            set { _cvc = value; }
+        }
+        private int _year;
+
+        public int Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+        private int _month;
+
+        public int Month
+        {
+            get { return _month; }
+            set { _month = value; }
+        }
+
+        public Card(string holder_name, double number, int cvc, int year, int month)
+        {
+            _holrer_name = holder_name;
+            _number = number;
+            _cvc = cvc;
+            _year = year;
+            _month = month;
+        }
+        public string Info
+        {
+            get
+            {
+                return $"{_number}  {_year}/{0}{_month}  {_holrer_name}  {_cvc}";
+            }
+        }
+    }
+}
---
+++ банки1/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace банки1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Card> _cards = new List<Card>();
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+       
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        { 
+            double number;
+            int month, year, cvc;
+            if (int.TryParse(textBoxCVC.Text, out cvc))
+            {
+                if (int.TryParse(textBoxMonth.Text, out month))
+                {
+                    if (int.TryParse(textBoxYear.Text, out year))
+                    {
+                        if (double.TryParse(textBoxNumber.Text, out number))
+                        {
+                            var card = new Card(textBoxName.Text, number, cvc, year, month);
+                            _cards.Add(card);
+                            listBoxCards.Items.Add(card.Info);
+                            textBoxCVC.Text = "";
+                            textBoxMonth.Text = "";
+                            textBoxName.Text = "";
+                            textBoxNumber.Text = "";
+                            textBoxYear.Text = "";
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Incorrect insert");
+            }
+
+        }
+        private void RefreshListBox()
+        {
+            listBoxCards.ItemsSource = null;
+            listBoxCards.ItemsSource = _cards;
+        }
+
+        private void buttonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBoxCards.SelectedIndex != -1)
+            {
+                _cards.RemoveAt(listBoxCards.SelectedIndex);
+                RefreshListBox();
+            }
+        }
+
+        private void listBoxCards_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            buttonDelete.IsEnabled = listBoxCards.SelectedIndex != -1;
+        }
+    }
+}
